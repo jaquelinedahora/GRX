f458eb7bf8c6054eba61c3048aa9d6f2cf217ef0
Jaqueline da Hora <82454233+jaquelinedahora@users.noreply.github.com>
2022-04-29 13:00:30 -0300

    Add files via upload

---
+++ grx.py
@@ -0,0 +1,44 @@
+# RESPOSTAS DOS QUESTIONÁRIO #
+
+# 1 - A
+# 2 - D
+# 3 - 20
+# 4 - 5
+# 5 -
+# 6 -
+# 7 - A sigla CRUD significa as iniciais das operações create (criação), read (leitura), update (atualização) e delete (exclusão).
+# Essas quatro siglas tratam a respeito das operações executadas em bancos de dados relacional (SQL) e não-relacional (NoSQL).
+# 8 -interface gráfica do usuário de um site, C++, CSS, HTML, JAVASCRIPT, PYTHON
+# 9 - se relaciona com o que está por trás das aplicações desenvolvidas na programação, NODEDS, JAVA, PYTHON, SQN, MYSQL,
+# 10 -
+# 11 -
+# 12 -
+# 13 -
+# 14 -
+# 15 -
+# 16 -
+# 17 -
+# 18 -
+# 19 -
+
+
+#
+#a = 10
+#i = 0
+
+#while(i <10):
+ # while(i <10):
+   # a = a + 1
+   # i = i + 1
+#print(a)
+#
+
+#a = 0
+#x = 0
+
+#for i in range(0,10):
+ # while x < 5:
+  #  x = x + 1
+  #  a = a + 1
+#print(a)
+
